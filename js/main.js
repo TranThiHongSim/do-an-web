1a5ef426a4d12646decde7270774bec5c4774962
Duongnhat512 <duongnhat512@gmail.com>
2024-04-04 21:46:15 +0700

    day header bang ajax

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    pushHeader()
+    function pushHeader(){
+        $.ajax({
+            url: 'header.html',
+            dataType: 'html',
+            success: function(data) {
+                $('.header-wrapped').html(data);
+            },
+            error: function() {
+            }
+        });
+    }
+
+    $('.nav-link').hover(function () {
+            $(this).addClass('hover')
+        }, function () {
+            var parent = $(this).parent()
+            if(!parent.hasClass('submenu')){
+                $(this).removeClass('hover')
+            }
+            else{
+                $('.submenu').mouseleave(function () {
+                    if (!$(this).find('.submenu-content').is(':hover')) {
+                        $(this).find('.nav-link').removeClass('hover');
+                    }
+                });
+            }
+        }
+        );
+
+})
